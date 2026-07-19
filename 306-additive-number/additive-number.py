f9af164f2a3a3155f52963e873eb781ec5995da0
BonamGokulVenkat <161847524+BonamGokulVenkat@users.noreply.github.com>
2026-07-19 18:37:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (30.88%) - LeetSync

--- 306-additive-number/additive-number.py
+++ 306-additive-number/additive-number.py
@@ -3,17 +3,18 @@ class Solution:
         n = len(num)
 
         for i in range(1, n):
+            
             if num[0] == '0' and i > 1:
                 break
 
-            first = int(num[:i])
-
             for j in range(i + 1, n):
+                
                 if num[i] == '0' and j - i > 1:
                     break
 
-                second = int(num[i:j])
-                a, b = first, second
+                a = int(num[:i])
+                b = int(num[i:j])
+
                 k = j
 
                 while k < n:
