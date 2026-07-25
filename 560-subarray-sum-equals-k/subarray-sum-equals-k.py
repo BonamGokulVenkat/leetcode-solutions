7006fb7518e2b1ff0598a46a84c8744b6be99c24
BonamGokulVenkat <161847524+BonamGokulVenkat@users.noreply.github.com>
2026-07-25 22:45:44 +0530

    Time: 46 ms (11.73%) | Memory: 23.5 MB (6.02%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefix_count = defaultdict(int)
+        prefix_count[0] = 1
+
+        prefix = 0
+        count = 0
+
+        for num in nums:
+            prefix += num
+
+            count += prefix_count[prefix - k]
+
+            prefix_count[prefix] += 1
+
+        return count
